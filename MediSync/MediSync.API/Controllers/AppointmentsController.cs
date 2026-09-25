51fc237658e914cdaf2ebed9eb09ce5adf6b6162
İsmail Umutluoğlu <ismailumutluoglu10@gmail.com>
2026-09-25 15:52:16 +0300

    feat: add AppointmentsController with GET and POST endpoints

---
+++ MediSync/MediSync.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using MediSync.Application.Features.Appointments.Commands.CreateAppointment;
+using MediSync.Application.Features.Appointments.Queries.GetAllAppointments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediSync.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AppointmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllAppointmentsQuery());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateAppointmentCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return Ok(id);
+        }
+    }
+}
