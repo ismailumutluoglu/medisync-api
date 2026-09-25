5b3a1c753b9093f92bd2a2d13c8dae4d860e6149
İsmail Umutluoğlu <ismailumutluoglu10@gmail.com>
2026-09-25 15:19:26 +0300

    feat: add GetAllAppointments CQRS query and handler

---
+++ MediSync/MediSync.Application/Features/Appointments/Queries/GetAllAppointments/GetAllAppointmentsHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using MediSync.Domain.Entities;
+
+namespace MediSync.Application.Features.Appointments.Queries.GetAllAppointments
+{
+    public class GetAllAppointmentsHandler : IRequestHandler<GetAllAppointmentsQuery, List<Appointment>>
+    {
+        public async Task<List<Appointment>> Handle(GetAllAppointmentsQuery request, CancellationToken cancellationToken)
+        {
+            // Repository buraya gelecek
+            return new List<Appointment>();
+        }
+    }
+}
---
+++ MediSync/MediSync.Application/Features/Appointments/Queries/GetAllAppointments/GetAllAppointmentsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MediSync.Domain.Entities;
+
+namespace MediSync.Application.Features.Appointments.Queries.GetAllAppointments
+{
+    public class GetAllAppointmentsQuery : IRequest<List<Appointment>>
+    {
+    }
+}
