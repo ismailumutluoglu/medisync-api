34dd82e622798adbaf3760a879e9d4e9973ffadf
İsmail Umutluoğlu <ismailumutluoglu10@gmail.com>
2026-09-25 15:16:40 +0300

    feat: add CreateAppointment CQRS command and handler

---
+++ MediSync/MediSync.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommand..cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace MediSync.Application.Features.Appointments.Commands.CreateAppointment
+{
+    public class CreateAppointmentCommand : IRequest<int>
+    {
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+}
---
+++ MediSync/MediSync.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MediSync.Domain.Entities;
+
+namespace MediSync.Application.Features.Appointments.Commands.CreateAppointment
+{
+    public class CreateAppointmentHandler : IRequestHandler<CreateAppointmentCommand, int>
+    {
+        public async Task<int> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
+        {
+            var appointment = new Appointment
+            {
+                PatientId = request.PatientId,
+                DoctorId = request.DoctorId,
+                AppointmentDate = request.AppointmentDate,
+                Notes = request.Notes,
+                Status = "Pending",
+                CreatedAt = DateTime.UtcNow
+            };
+
+            // Repository buraya gelecek
+            return appointment.Id;
+        }
+    }
+}
